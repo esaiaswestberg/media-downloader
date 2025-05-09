41d27386db2e2597ed08bb241a8be1fa596ca55d
Esaias Westberg <esaias@westbergs.se>
2025-05-10 00:28:34 +0200

    Add @tanstack/react-router for routing, update entry point, and configure Vite plugin for improved routing capabilities

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,15 +3,23 @@ import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from './components/theme-provider'
-import { ModeToggle } from './components/mode-toggle'
+import { createRouter, Router, RouterProvider } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
 
 const queryClient = new QueryClient()
+const router = createRouter({routeTree})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof Router
+  }
+}
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme='system' storageKey='media-downloader-theme'>
-        <ModeToggle />
+        <RouterProvider router={router} />
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
