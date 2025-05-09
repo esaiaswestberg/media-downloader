bc603512f314e7d812e907d11c3bd201fa841d57
Esaias Westberg <esaias@westbergs.se>
2025-05-10 00:20:17 +0200

    Add light/dark theming

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,17 @@ import { StrictMode } from 'react'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from './components/theme-provider'
+import { ModeToggle } from './components/mode-toggle'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <h1>Hello, world!</h1>
+      <ThemeProvider defaultTheme='system' storageKey='media-downloader-theme'>
+        <ModeToggle />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
