cf23414011702afd8d175eaa7b7ac92580bf2a86
Esaias Westberg <esaias@westbergs.se>
2025-05-10 00:16:45 +0200

    Add @tanstack/react-query for data fetching; update app title and structure for new functionality. Remove unused assets

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createRoot } from 'react-dom/client'
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <h1>Hello, world!</h1>
+    </QueryClientProvider>
   </StrictMode>,
 )
